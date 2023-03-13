7f57b872e396f6c4026ab423b03ecb35bd4bdeeb
IwannaBerealnerD <iwannaberealnerd@gmail.com>
2023-03-13 09:48:55 +0900

    [init] 재시작

--- src/components/testComponent/index.tsx
+++
@@ -1,9 +0,0 @@
-import { wrapper } from "./style.css";
-
-export const TestComponent = () => {
-  return (
-    <div className={wrapper}>
-      <p>test</p>
-    </div>
-  );
-};
--- src/components/testComponent/style.css.ts
+++
@@ -1,6 +0,0 @@
-import { style } from "@vanilla-extract/css";
-
-export const wrapper = style({
-  padding: 10,
-  backgroundColor: "pink",
-});
