826b6e9e388cb24c60687f633d5aef14ebcb2695
IwannaBerealnerD <iwannaberealnerd@gmail.com>
2023-03-17 14:09:33 +0900

    [feat] test 추가

---
+++ apps/nextjs-playground/src/utils/testFunc2.ts
@@ -0,0 +1,26 @@
+export const testFunc2 = () => {
+  const largeArray = [];
+  for (let i = 0; i < 100000; i++) {
+    largeArray.push(
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".repeat(
+        10
+      )
+    );
+  }
+  for (let i = 0; i < 100000; i++) {
+    largeArray.push(
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".repeat(
+        10
+      )
+    );
+  }
+  for (let i = 0; i < 100000; i++) {
+    largeArray.push(
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".repeat(
+        10
+      )
+    );
+  }
+
+  return largeArray;
+};
