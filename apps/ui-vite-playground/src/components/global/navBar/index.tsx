ea89930ee036863f3cf8261597277d7e2060b3c6
IwannaBerealnerD <iwannaberealnerd@gmail.com>
2023-03-24 23:53:37 +0900

    [vite-ui/feat] unmount 시 애니메이션 적용

--- apps/ui-vite-playground/src/components/global/navBar/index.tsx
+++ apps/ui-vite-playground/src/components/global/navBar/index.tsx
@@ -1,42 +1,73 @@
 import { AiOutlineHome, AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
 import { GoThreeBars } from "react-icons/go";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-import { cssObj } from "./style.css";
+import { cssObj, searchBoxOpened, searchBoxClosed } from "./style.css";
+import { CAR_ARR } from "./constant";
 
 export const NavBar = () => {
+  const [search, setSearch] = useState("");
+
   const [isDropBoxOpen, setIsDropBoxOpen] = useState(false);
+  const [isTirgerred, setIsTriggered] = useState(false);
   const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [isSearchAnimation, setIsSearchAnimation] = useState(false);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (isTirgerred) {
+      setIsSearchOpen(true);
+      setIsSearchAnimation(true);
+      return;
+    }
+    setIsSearchAnimation(false);
+    timer = setTimeout(() => setIsSearchOpen(false), 400);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [isTirgerred]);
+
+  const filteredCars = CAR_ARR.filter((car) => search.length && car.name.toLowerCase().includes(search.toLowerCase()));
 
   return (
-    <nav>
-      <div className={cssObj.container}>
-        <h1 className={cssObj.heading}>DashBoard</h1>
-        <div className={cssObj.iconContainer}>
-          <button type="button" className={cssObj.iconButton}>
-            <AiOutlineHome />
-          </button>
-
-          <input className={isSearchOpen ? cssObj.searchBoxClosed : cssObj.searchBoxOpened} />
-          <button
-            type="button"
-            className={cssObj.iconButton}
-            onClick={() => {
-              setIsSearchOpen((prevValue) => !prevValue);
-            }}
-          >
-            {isSearchOpen ? <AiOutlineSearch /> : <AiOutlineClose />}
-          </button>
-          <button
-            type="button"
-            className={cssObj.iconButton}
-            onClick={() => {
-              setIsDropBoxOpen((prevValue) => !prevValue);
-            }}
-          >
-            <GoThreeBars />
-          </button>
+    <nav className={cssObj.wrapper}>
+      <h1 className={cssObj.heading}>DashBoard</h1>
+      <div className={cssObj.inputContainer}>
+        <div className={cssObj.container}>
+          <div className={cssObj.testContainer}>
+            <button
+              type="button"
+              className={cssObj.iconButton}
+              onClick={() => {
+                setIsTriggered((prevValue) => !prevValue);
+              }}
+            >
+              {isSearchAnimation ? <AiOutlineClose /> : <AiOutlineSearch />}
+            </button>
+            {(isSearchOpen || isSearchAnimation) && (
+              <div className={isSearchAnimation ? searchBoxOpened : searchBoxClosed}>
+                <input
+                  className={cssObj.inputBox}
+                  placeholder="입력하세요"
+                  onChange={(e) => {
+                    setSearch(e.target.value);
+                  }}
+                />
+                {Boolean(filteredCars.length) &&
+                  filteredCars?.map((car, index) => index < 3 && <button key={car.name}>{car.name}</button>)}
+              </div>
+            )}
+          </div>
         </div>
+        <button
+          type="button"
+          className={cssObj.iconButton}
+          onClick={() => {
+            setIsDropBoxOpen((prevValue) => !prevValue);
+          }}
+        >
+          <GoThreeBars />
+        </button>
       </div>
     </nav>
   );
--- apps/ui-vite-playground/src/components/global/navBar/style.css.ts
+++ apps/ui-vite-playground/src/components/global/navBar/style.css.ts
@@ -1,18 +1,76 @@
-import { style } from "@vanilla-extract/css";
+import { style, keyframes } from "@vanilla-extract/css";
+
+const widthGrowKeyframe = keyframes({ "0%": { width: "0", opacity: 0 }, "100%": { width: "10rem", opacity: 1 } });
+const widthShrinkKeyframe = keyframes({ "0%": { width: "10rem", opacity: 1 }, "100%": { width: "0", opacity: 0 } });
 
 export const cssObj = {
-  container: style({
+  wrapper: style({
     display: "flex",
-    alignContent: "center",
+    alignItems: "center",
     justifyContent: "space-between",
-    backgroundColor: "pink",
+    height: "3rem",
+    borderBottom: "1px solid #111111",
   }),
   heading: style({
-    backgroundColor: "gray",
     fontSize: "1.5rem",
   }),
-  iconContainer: style({ display: "flex", alignItems: "center" }),
-  iconButton: style({ fontSize: "1.3rem", backgroundColor: "red", display: "flex", alignItems: "center" }),
-  searchBoxClosed: style({ width: 0, transition: "0.4s" }),
-  searchBoxOpened: style({ width: "10rem", transition: "0.4s" }),
+  inputContainer: style({
+    display: "flex",
+    alignItems: "center",
+    gap: "0.5rem",
+    height: "100%",
+    backgroundColor: "blue",
+  }),
+  container: style({ position: "relative", height: "100%" }),
+  iconButton: style({
+    fontSize: "1.3rem",
+    display: "flex",
+    alignItems: "center",
+    transition: "all, 0.3s",
+    padding: "0.3rem",
+    position: "relative",
+    zIndex: "1",
+    // height: "100%",
+    ":hover": { backgroundColor: "rgba(1,1,1,0.2)", borderRadius: "50%" },
+  }),
+  inputBox: style({ width: "100%" }),
+
+  testContainer: style({
+    backgroundColor: "pink",
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+  }),
 };
+
+const searchBox = style({
+  minHeight: "2.5rem",
+  padding: "0.8rem",
+  height: "auto",
+  borderRadius: "2rem",
+  backgroundColor: "rgba(1,1,1,0.2)",
+  position: "absolute",
+  right: 0,
+  top: 0,
+  //  position: "absolute", top: "50%"
+  // top: "50%",
+  // transform: "translateY(-50%)",
+});
+
+export const searchBoxClosed = style([
+  searchBox,
+  {
+    animationName: widthShrinkKeyframe,
+    animationDuration: "0.3s",
+    animationFillMode: "forwards",
+  },
+]);
+
+export const searchBoxOpened = style([
+  searchBox,
+  {
+    animationName: widthGrowKeyframe,
+    animationDuration: "0.3s",
+    animationFillMode: "forwards",
+  },
+]);
