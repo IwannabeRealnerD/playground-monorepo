c9ba3f2ad18fa11871241f0033c1676d6ef186ef
IwannabeRealnerD <IwannabeRealnerD@gmail.com>
2023-07-11 09:10:10 +0900

    [setting-feat] eslint base 적용 완료

---
+++ settings/eslint-config-khanne-base/index.js
@@ -0,0 +1,36 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: ["plugin:@typescript-eslint/recommended"],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+  plugins: ["import"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-unused-vars": "error",
+    "import/extensions": ["off"],
+    "import/prefer-default-export": "off",
+    "import/no-unresolved": "off",
+    "arrow-body-style": ["error", "as-needed"],
+    "no-console": "error",
+    camelcase: "off",
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: [
+          "test.{ts,tsx}",
+          "test-*.{ts,tsx}",
+          "**/*{.,_}{test,spec}.{ts,tsx}",
+          "**/jest.config.ts", // jest config
+          "**/jest.setup.ts", // jest setup
+        ],
+        optionalDependencies: false,
+      },
+    ],
+  },
+};
